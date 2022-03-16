9d5b24f4535d7767dc3feacbf30aefab38801f3b
Cameron Fetter <cameronfetter777@gmail.com>
2022-03-16 15:20:22 -0400

    yooo

--- monsterfy.py
+++ monsterfy.py
@@ -2,6 +2,14 @@ import csv
 import json
 
 ms = []
+# ms[0] will be the string to construct the variable
+# ms[1] will be an array of types
+
+typeslist = []
+# all types in everything
+
+typeslist_pretty = []
+# prettified
 
 with open('monsters.csv', newline='') as csvfile:
 		mreader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -10,9 +18,21 @@ with open('monsters.csv', newline='') as csvfile:
 			if skip:
 				skip = False
 			else:
-				ms.append("'"+row[0]+"'")
+				adder = '["'+row[0]+'",['
+				types = row[1].split('/')
+				types2 = []
+				for t in types:
+					types2.append('"'+t+'"')
+					if t not in typeslist:
+						typeslist.append(t)
+						typeslist_pretty.append('"'+t+'"')
+				adder += ",".join(types2)
+				adder += "]]"
+				ms.append(adder)
 
-monster_string = "var MONSTERS = [" + ", ".join(ms) + "];"
+monster_string = "var MONSTERS = [" + ", ".join(ms) + "];\n" + "var ALLTYPES = [" + ",".join(typeslist_pretty)+"];"
+# build a string to write a variable to file
+# so hacky
 
 
 
